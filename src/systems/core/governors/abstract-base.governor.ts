8bc27ea48b22ce5fc176a1c8151b8a953b745a2a
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-13 17:50:57 -0400

    Governor flexibility

--- src/systems/core/governors/abstract-base.governor.ts
+++ src/systems/core/governors/abstract-base.governor.ts
@@ -1,6 +1,6 @@
-import { AtomicModuleInterface } from "../interfaces/atomic-module.interface";
+import type { AtomicModuleInterface } from "../interfaces/atomic-module.interface";
 
-export abstract class AbstractBaseGovernor {
+export abstract class AbstractBaseGovernor<ModuleType = AtomicModuleInterface> {
   constructor(
     protected name: string,
     protected side: string,
@@ -10,7 +10,7 @@ export abstract class AbstractBaseGovernor {
    * Register this module for use
    * @param module The module to use
    */
-  abstract useModule(module: AtomicModuleInterface): this;
+  abstract useModule(module: ModuleType): this;
   
   /**
    * Start whatever this system does
