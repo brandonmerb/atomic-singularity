779a5cc44cc855affec8fcae90dc931d31708087
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-13 18:49:08 -0400

    Bug with flexible module types

--- src/systems/core/helpers/utility.helpers.ts
+++ src/systems/core/helpers/utility.helpers.ts
@@ -1,7 +1,7 @@
 import { AtomicSingularitySystem } from "../atomic-singularity.system";
 import { AtomicModuleInterface } from "../interfaces/atomic-module.interface";
 
-export function createModule(options: AtomicModuleInterface): AtomicModuleInterface {
+export function createModule<ModuleType extends AtomicModuleInterface>(options: ModuleType): ModuleType {
   return options;
 }
 
