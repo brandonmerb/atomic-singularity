e494f03d3d36ceff281a7e88b0ddd6e7132a7ddc
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-25 10:26:50 -0400

    Type cleanup

--- src/types/executor-functions.types.ts
+++ src/types/executor-functions.types.ts
@@ -2,6 +2,10 @@ import { AtomicNebulaInterface } from "../interfaces/atomic-nebula.interface";
 import { AtomicSingularitySystem } from "../atomic-singularity.system";
 
 export type ExecutorFunction<NebulaType extends AtomicNebulaInterface = AtomicNebulaInterface> = 
-  (app?: AtomicSingularitySystem, nebula?: NebulaType) => boolean | void | null | undefined;
+  ((app: AtomicSingularitySystem, nebula: NebulaType) => boolean | void | null | undefined) | 
+  ((app: AtomicSingularitySystem) => boolean | void | null | undefined) | 
+  (() => boolean | void | null | undefined);
 
-export type AsyncActivationFunction<NebulaType extends AtomicNebulaInterface = AtomicNebulaInterface> = (nebula?: NebulaType) => Promise<boolean>;
+export type AsyncActivationFunction<NebulaType extends AtomicNebulaInterface = AtomicNebulaInterface> =
+  ((nebula: NebulaType) => Promise<boolean>) | 
+  (() => Promise<boolean>);
